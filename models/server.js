164f7562add47f314a664a59c5c3af6f01f79053
Sebasmedcol <sebastiaupegui1@gmail.com>
2024-09-10 01:17:06 -0500

    segundo commit

--- models/server.js
+++ models/server.js
@@ -9,18 +9,28 @@ import celdaRouter from '../routers/celdaRouter.js'; // Rutas de celdas
 export default class Server {
     constructor() {
         this.app = express();
+        this.middlewares(); // Agregar middlewares
         this.listen();
         this.dbConnect();
         this.pathCelda = '/api/celdas'; // Definir el path para las rutas de celdas
         this.routes();
-
     }
+
+    middlewares() {
+        // Middleware para manejar JSON
+        this.app.use(express.json());
+        // Middleware para manejar datos URL-encoded
+        this.app.use(express.urlencoded({ extended: true }));
+        // Puedes agregar más middlewares aquí si es necesario
+    }
+
     listen() {
         // Levantar el servidor en el puerto definido
-        this.app.listen(process.env.PORT,() => {
+        this.app.listen(process.env.PORT, () => {
             console.log(`Server is running on port ${process.env.PORT}`);
         });
     }
+
     async dbConnect() {
         await dbConnection();
     }
